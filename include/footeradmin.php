8fdc6d7adb04e6c1002974f04f3800cfcf9a253b
PAPKung <mr.mathuskhetanan@gmail.com>
2024-11-27 15:36:29 +0700

    up

---
+++ include/footeradmin.php
@@ -0,0 +1,7 @@
+<!-- footer -->
+</div>
+<footer class="bg-light text-center py-3 fixed-bottom">footer</footer>
+<script src="../js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+<!-- end footer -->
